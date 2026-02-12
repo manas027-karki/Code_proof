d2b19c31406833e1cfff95b800157e12e48a17fc
Nithin0620 <nithin30302@gmail.com>
2026-02-12 16:08:18 +0530

    ..

--- cli/commands/help.js
+++ cli/commands/help.js
@@ -7,21 +7,17 @@ import { formatBlock } from "../ui/formatting.js";
 const HELP_TEXT = `Usage: codeproof <command>
 
 Commands:
-  init               Initialize CodeProof in a Git repository
-  run                Run CodeProof checks
-  report@dashboard   Send latest report and show dashboard link
-  move-secret        Move high-risk secrets to .env
-  ignore             Temporarily disable commit enforcement
-  apply              Re-enable commit enforcement
-  whoami             Show the local CodeProof client ID
-  help               Show this help menu
-  -h, --help         Show this help menu
+  codeproof init               Initialize CodeProof in a Git repository
+  codeproof run                Run CodeProof checks
+  codeproof report@dashboard   Send latest report and show dashboard link
+  codeproof move-secret        Move high-risk secrets to .env
+  codeproof ignore             Temporarily disable commit enforcement
+  codeproof apply              Re-enable commit enforcement
+  codeproof whoami             Show the local CodeProof client ID
+  codeproof help               Show this help menu
+  codeproof -h, --help         Show this help menu
 
-Examples:
-  codeproof init
-  codeproof run
-  codeproof move-secret --yes
-  codeproof help
+
 `;
 
 /**
